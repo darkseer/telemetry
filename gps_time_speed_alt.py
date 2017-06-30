179cdd00013805d9be343b467feeebc27a94062c
Glenn Buckholz <glenn.buckholz@coveros.com>
2017-06-29 23:30:44 -0400

    Added the shebangs

--- gps_time_speed_alt.py
+++ gps_time_speed_alt.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import gps
 from subprocess import call
 import time
--- video_collect.py
+++ video_collect.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
+
 from subprocess import call
 import time
 from datetime import datetime, date, time
 today = datetime.today()
 filename= today.strftime("%Y%m%d%H%M%S")
 print filename
-call(["raspivid","-t","0","-o","skycast_"+filename+".mp4"])
+call(["raspivid","-t","0","-o","filename"+"_skycast.mp4"])
